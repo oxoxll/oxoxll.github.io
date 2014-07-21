46a8fcb65d7ee0a1e070450508dfa565dd5cd4ef
oxoxll <oxoxll@qq.com>
2014-07-21 14:53:56 +0800

    test

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log(1);
+var websocket = new WebSocket('ws://111.206.45.12:30201');
+websocket.onopen = function(evt) {
+    console.log('onopen');
+}; 
+websocket.onclose = function(evt) {
+    console.log('onclose');
+}; 
+websocket.onmessage = function(evt) {
+    console.log('onmessage');
+}; 
+websocket.onerror = function(evt) {
+    console.log('onerror');
+};
